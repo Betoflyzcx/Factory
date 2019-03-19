ece30d30fb00e1891550b9391daf0a1aaf6bc5d3
Alberto Garcia <albertogarcia1268@yahoo.com>
2019-03-18 21:20:05 -0700

    Updated Gtest

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "vehicle-types.h"
+#include "gtest/gtest.h"
+#include "string.h"
+
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
